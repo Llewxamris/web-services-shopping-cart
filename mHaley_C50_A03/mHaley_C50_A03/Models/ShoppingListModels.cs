ab67e6082798eea13bf7f02e1bf3f9e92c7aa262
Llewxamris <maxwell.haley@openmailbox.org>
2017-11-17 18:29:40 -0500

    Add base singleton pattern to ShoppingList
    Add closing tag comments.

--- mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
+++ mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
@@ -10,18 +10,27 @@ namespace mHaley_C50_A03.Models
         /* Mimics a shopping list. Contains a list of ShoppingListEntries.
          * Also contains a singleton pattern that stores all this list into
          * memory. */
-        public List<ShoppingListEntry> Entries { get; set; }
-        // TODO: Singleton Pattern
-    }
+        private static List<ShoppingListEntry> _instance;
+
+        public static List<ShoppingListEntry> Entries => _instance ??
+            (_instance = GetShoppingListFromXml());
+
+        private static List<ShoppingListEntry> GetShoppingListFromXml()
+        {
+            return null;
+        }
+    }// ShoppingList
 
     public class ShoppingListEntry
     {
+        /* Mimics an entry in a shopping list. Is stored in a list by the
+         * ShoppingList class.*/
         public int Quantity { get; set; }
         public string ProductName { get; set; }
         public decimal ProductPrice { get; set; }
         public string ProductCategory { get; set; }
 
-        public ShoppingListEntry() { }
+        public ShoppingListEntry() { } // ShoppingListEntry()
 
         public ShoppingListEntry(int quantiy, string productName,
             decimal productPrice, string productCategory)
@@ -30,6 +39,6 @@ namespace mHaley_C50_A03.Models
             ProductName = productName;
             ProductPrice = productPrice;
             ProductCategory = productCategory;
-        }
-    }
+        }// ShoppingListEntry(...)
+    }// ShoppingListEntry
 }
