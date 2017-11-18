f5cb55357140d6572a27e9fe413a9f8b062793e2
Llewxamris <maxwell.haley@openmailbox.org>
2017-11-17 20:57:59 -0500

    Retrieve data from XML file, display in List
    The XML file is parsed, and all entires are shown in a
    MVC list view.

--- mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
+++ mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
@@ -1,7 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Web;
+using System.Web.Hosting;
+using System.Xml.Linq;
+using WebGrease.Css.Extensions;
 
 namespace mHaley_C50_A03.Models
 {
@@ -15,16 +22,42 @@ namespace mHaley_C50_A03.Models
         public static List<ShoppingListEntry> Entries => _instance ??
             (_instance = GetShoppingListFromXml());
 
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
         private static List<ShoppingListEntry> GetShoppingListFromXml()
         {
-            return null;
-        }
+            /* Read the ShoppingList.xml file, and convert each entry to an
+             * object of ShoppingListEntry. Returns each object in a list. */
+            try
+            {
+                var entries = new List<ShoppingListEntry>();
+                // ReSharper disable once AssignNullToNotNullAttribute
+                XElement shoppingListXmlElement = XElement.Load(HostingEnvironment.MapPath("~/App_Data/ShoppingList.xml"));
+                IEnumerable<XElement> entryElements =
+                    from x in shoppingListXmlElement.Elements()
+                    select x;
+
+                entryElements.ForEach(entry =>
+                {
+                    entries.Add(new ShoppingListEntry(int.Parse(entry.Element("quantity").Value),
+                        entry.Element("productName").Value, 
+                        decimal.Parse(entry.Element("productPrice").Value),
+                        entry.Element("productCategory").Value));
+                });
+
+                return entries;
+            }
+            catch (IOException)
+            {
+                return null;
+            }
+        }// GetShoppingListFromXml()
     }// ShoppingList
 
     public class ShoppingListEntry
     {
         /* Mimics an entry in a shopping list. Is stored in a list by the
          * ShoppingList class.*/
+
         public int Quantity { get; set; }
         public string ProductName { get; set; }
         public decimal ProductPrice { get; set; }
