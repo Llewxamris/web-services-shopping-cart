e534f75cfc9eba3d68ae6a6ad2beee2c229d2f96
Llewxamris <maxwell.haley@openmailbox.org>
2017-11-17 21:40:24 -0500

    Fix ShoppingList.xml to follow the proper schema
    Add annotations for the ShoppingListEntry

--- mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
+++ mHaley_C50_A03/mHaley_C50_A03/Models/ShoppingListModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
@@ -39,9 +40,9 @@ namespace mHaley_C50_A03.Models
                 entryElements.ForEach(entry =>
                 {
                     entries.Add(new ShoppingListEntry(int.Parse(entry.Element("quantity").Value),
-                        entry.Element("productName").Value, 
-                        decimal.Parse(entry.Element("productPrice").Value),
-                        entry.Element("productCategory").Value));
+                        entry.Element("product").Element("name").Value, 
+                        decimal.Parse(entry.Element("product").Element("price").Value),
+                        entry.Element("product").Attribute("category").Value.ToString()));
                 });
 
                 return entries;
@@ -57,10 +58,23 @@ namespace mHaley_C50_A03.Models
     {
         /* Mimics an entry in a shopping list. Is stored in a list by the
          * ShoppingList class.*/
-
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least one.")]
+        [DisplayName("Quantity")]
         public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required.")]
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [DisplayName("Price")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency)]
         public decimal ProductPrice { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        [DisplayName("Category")]
         public string ProductCategory { get; set; }
 
         public ShoppingListEntry() { } // ShoppingListEntry()
