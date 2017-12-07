17b91de296f93006e25711a01d59d37e509e6c63
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-12-07 01:23:55 -0500

    Use proper contoller class
    Now uses a proper Web API controller class instead
    of using the controller created in the previous
    assignemnt.

---
+++ mHaley_C50_A03/mHaley_C50_A03/Controllers/RestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using mHaley_C50_A03.Models;
+
+namespace mHaley_C50_A03.Controllers
+{
+    public class RestController : ApiController
+    {
+        // GET: api/Rest
+        public List<ShoppingListEntry> Get()
+        {
+            return ShoppingList.GetAllEntries();
+        }
+
+        // GET: api/Rest/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Rest
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Rest/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Rest/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
