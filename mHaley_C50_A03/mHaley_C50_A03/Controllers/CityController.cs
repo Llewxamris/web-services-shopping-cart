ee7a755e986158d5a04022ff4f6ff646e294feb4
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-12-09 05:29:46 -0500

    Add fetching city information

--- mHaley_C50_A03/mHaley_C50_A03/Controllers/CityController.cs
+++ mHaley_C50_A03/mHaley_C50_A03/Controllers/CityController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using mHaley_C50_A03.CitiesService;
 
 namespace mHaley_C50_A03.Controllers
@@ -23,6 +24,32 @@ namespace mHaley_C50_A03.Controllers
             CityDetail[] cities = _citiesSoap.GetCities(countryCode, "10");
             Dictionary<string, string> citiesDictionary = cities.ToDictionary(cityDetail => cityDetail._id.ToString(), cityDetail => cityDetail.name);
             return Json(citiesDictionary, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetCityDetails(string cityCode)
+        {
+            CityDetail cityLocation = _citiesSoap.GetCityDetail(int.Parse(cityCode));
+            XElement cityWeatherXElement = _citiesSoap.GetCityWeather(int.Parse(cityCode));
+
+            IEnumerable<string> cityWeatherValue = cityWeatherXElement.Elements("temperature")
+                .Select(i => i.Attribute("value").Value);
+
+            IEnumerable<string> cityWeatherMetric = cityWeatherXElement.Elements("temperature")
+                .Select(i => i.Attribute("unit").Value);
+
+            char cityWeatherMetricChar = cityWeatherMetric.ElementAt(0) == "metric" ? 'C' : 'F';
+            string cityTemperature = cityWeatherValue.ElementAt(0) + " " + cityWeatherMetricChar;
+
+
+            // Theres more weather data available, but it would just be several
+            // repeats of the above selectors.
+            return Json(new List<string>
+            {
+                cityLocation.coord.lat.ToString(),
+                cityLocation.coord.lon.ToString(),
+                cityTemperature
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
