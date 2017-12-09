aa675524d0c322670113ad8d95779704a9fecdd7
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-12-09 04:10:13 -0500

    Create select element for each city in country

--- mHaley_C50_A03/mHaley_C50_A03/Controllers/CityController.cs
+++ mHaley_C50_A03/mHaley_C50_A03/Controllers/CityController.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using mHaley_C50_A03.CitiesService;
 
 namespace mHaley_C50_A03.Controllers
 {
     public class CityController : Controller
     {
+        private readonly CitiesSoapClient _citiesSoap = new CitiesSoapClient();
         // GET: City
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetCities(string countryCode)
+        {
+            CityDetail[] cities = _citiesSoap.GetCities(countryCode, "10");
+            Dictionary<string, string> citiesDictionary = cities.ToDictionary(cityDetail => cityDetail._id.ToString(), cityDetail => cityDetail.name);
+            return Json(citiesDictionary, JsonRequestBehavior.AllowGet);
         }
     }
 }
